7eb8800be4ae3771eb082f52bdec8d34297042e3
Erick Cardenas <erick.mendez@toptal.com>
2024-03-13 16:50:22 -0400

    end of chapter 22

---
+++ monthly_challenges/challenges/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("This works!")
+
+
+def february(request):
+    return HttpResponse("February view")
