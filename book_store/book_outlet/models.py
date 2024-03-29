7b5ba928e25ec02050b1615616a5138b4b4b75d2
Erick Cardenas <erick.mendez@toptal.com>
2024-03-29 17:42:27 -0400

    end of chapter 94

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Book(models.Model):
@@ -9,6 +11,14 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404, render
+
+from .models import Book
 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    agg = books.aggregate(Avg("rating"))
+    return render(
+        request,
+        "book_outlet/index.html",
+        {"books": books, "num_books": num_books, "agg": agg},
+    )
+
+
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    return render(
+        request,
+        "book_outlet/book_detail.html",
+        {
+            "title": book.title,
+            "author": book.author,
+            "rating": book.rating,
+            "is_bestseller": book.is_bestselling,
+        },
+    )
